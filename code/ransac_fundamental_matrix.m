aee99323a160bea8cdaae137896c241078fc3316
Fahad Idrees <fahad@dapperapps.com.au>
2016-01-04 21:58:59 +0500

    comment added

--- code/ransac_fundamental_matrix.m
+++ code/ransac_fundamental_matrix.m
@@ -23,20 +23,35 @@
 % right images.
 
 function [ Best_Fmatrix, inliers_a, inliers_b] = ransac_fundamental_matrix(matches_a, matches_b)
+% row and column specify the size of new matrix which will be use to
+% generate the random matrix of indices.
 [row column] = size(matches_a);
+% d accomadate the matrix of indices which we use to fetch the points from
+% the matches_a and matches_b
 d = transpose(randsample(row,row));
+% patchmatrix1 and patchmatrix2 is used to fetch the 8*1 points from the matches_a and matches_b 
 patchmatrix1 = zeros(8,2);
 patchmatrix2 = zeros(8,2);
+% Best_Fmatrix will be the fundamental matrix with best inliers in the
+% array of points
 Best_Fmatrix = zeros(3,3);
+% variable to store the maximum values of best inliers in matches_a and matches_b
 max_of_fm = 0;
+% i and j will be use to fetch the chunk from d matrix of indices/
 i = 1;
 j = 8;
  for x = (1:103)
+    % 1*8 matrix of points for fundamental matrix
     points_a = d(i:j);
+    % get the 8 points from matches_a
     patchmatrix1=matches_a(points_a,:);
+    % get the 8 points from matches_b
     patchmatrix2=matches_b(points_a,:);
-     i = i+8;
-     j = j+8;
+    % increment to next chunk of the points
+    i = i+8;
+    j = j+8;
+    % calculate the fundametal matrix of 8*8 points in matches_a and
+    % matches_b
     F_matrix = estimate_fundamental_matrix(patchmatrix1, patchmatrix2);
     [U,S,V] = svd(F_matrix);
     M_norm_B=V(:,end);
@@ -52,7 +67,7 @@ j = 8;
         [u,v] = deal(tu{:});
         vw = num2cell(matches_b(vt:vt,1:end));
         [u_dash,v_dash] = deal(vw{:});
-        if ([u v 1] * M_norm_B * transpose([u_dash v_dash 1]) > 1.5)
+        if abs([u v 1] * M_norm_B * transpose([u_dash v_dash 1]) < 0.001)
             number_of_matches = number_of_matches + 1;
             inliersA(inliersCount:inliersCount,1:end) = [u v];
             inliersB(inliersCount:inliersCount,1:end) = [u_dash v_dash];
